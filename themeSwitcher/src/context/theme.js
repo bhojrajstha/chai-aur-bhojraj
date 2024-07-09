ffbb96c5b5ae893b8ded81862914b9f707587041
bhojrajstha <bhojrajshrestha770@gmail.com>
2024-07-09 21:55:36 +0545

    themeSwitcher updated

---
+++ themeSwitcher/src/context/theme.js
@@ -0,0 +1,14 @@
+import { useContext,createContext } from "react";
+
+export  const themeContext = createContext({
+    themeMode : "light",
+    lightMode: () => {},
+    darkMode: () => {},
+
+})
+
+export const ThemeProvider = themeContext.Provider;
+
+export default function useTheme() {
+    return useContext(themeContext)
+}
